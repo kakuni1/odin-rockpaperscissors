fb1d8452d40203607d1b151013cf1c27d7e55522
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2025-10-21 21:44:58 -0700

    add computer's choice

--- rps.js
+++ rps.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+  const n = Math.random();
+  return n < 1 / 3 ? "rock" : n < 2 / 3 ? "paper" : "scissor";
+}
+
+console.log(getComputerChoice());
