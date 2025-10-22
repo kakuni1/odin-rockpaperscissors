9645ef6d2fe66a03bd276715549888845d1b5b20
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2025-10-21 22:44:32 -0700

    add human's choice

--- rps.js
+++ rps.js
@@ -3,4 +3,11 @@ function getComputerChoice() {
   return n < 1 / 3 ? "rock" : n < 2 / 3 ? "paper" : "scissor";
 }
 
+function getHumanChoice() {
+  const input = prompt("Rock Paper Scissors (choose one)");
+  let lowerInput = input.toLowerCase();
+  return lowerInput;
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
